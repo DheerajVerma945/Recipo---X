aefb5a7ffc6ef046b6bf459e8f76224cf9a4d98d
Dheeraj Verma <vermadheeraj945@gmail.com>
2024-09-20 23:37:07 +0530

    Corrected env variables declaration

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,5 +1,5 @@
 const appWriteURL = String(import.meta.env.VITE_APPWRITE_URL);
-const appWriteProjectId = String(import.meta.env.VITE_APPWRITE_PROJECT_IDL).trim();
+const appWriteProjectId = String(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 const appWriteDbId = String(import.meta.env.VITE_APPWRITE_DATABASE_ID);
 const appWriteUserCollectionId = String(import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID);
 const appWritePostBucketId = String(import.meta.env.VITE_APPWRITE_MEAL_BUCKET_ID);
